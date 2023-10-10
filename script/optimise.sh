1122a71089accd076cf23ebddfbdff2db6853354
minhtoo <linmin.htoo@gmail.com>
2023-10-10 19:44:38 +0000

    feat: add bash script

---
+++ script/optimise.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+time_hash=$(date +%s)
+
+# RUN THIS FROM ROOT OF GITHUB REPO
+
+# store STDOUT
+mkdir -p logs/
+
+run_name="100parts_10steps_5res_5poses_8exh_v1_top100PRTs"
+python3 optimise/run.py \
+    --run_name ${run_name} \
+    --num_particles 100 \
+    --num_epochs 10 \
+    --out_folder "results" \
+    --scaffold "CNC(=O)C1=C(NC2=CC=CC=C2OC)C=C(NC(=O)C2CC2)N=N1" \
+    --residues_of_interest "VAL629" "ARG715" "TRP718" "THR555" "CYS675" \
+    --necessary_residues "VAL629" \
+    --ref_smi_excel_path "data/JAK2_IC50_data_Sep18.xlsx" \
+    --protein_pdb_path "data/mutant_PRT1008596.align.fixed.addH.pdb" \
+    --autobox_ligand_pdb_path "data/PRT1008596.sdf" \
+    2>&1 | tee logs/${run_name}_${time_hash}.log
