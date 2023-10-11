a3a74fc42593a6599697be16dbc7188c317582cd
minhtoo <linmin.htoo@gmail.com>
2023-10-11 09:14:50 +0000

    feat: add bash script

--- script/optimise_v2.sh
+++ script/optimise_v2.sh
@@ -7,11 +7,15 @@ export TF_CPP_MIN_LOG_LEVEL=3
 # store STDOUT
 mkdir -p logs/
 
-run_name="100parts_10steps_5res_5poses_8exh_v1_top100PRTs"
+# fixed hyperball clipping of latent vectors
+run_name="100parts_10steps_5res_10poses_8exh_seed42_top20smis_fixed"
 python3 optimise/run.py \
     --run_name ${run_name} \
     --num_particles 100 \
     --num_epochs 10 \
+    --seed 42 \
+    --num_top_smiles_to_init 20 \
+    --num_poses 10 \
     --out_folder "results" \
     --scaffold "CNC(=O)C1=C(NC2=CC=CC=C2OC)C=C(NC(=O)C2CC2)N=N1" \
     --residues_of_interest "VAL629" "ARG715" "TRP718" "THR555" "CYS675" \
--- script/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-time_hash=$(date +%s)
-python3 /home/minhtoo/molecule-generation/optimise/run.py \
-    2>&1 | tee /home/minhtoo/molecule-generation/logs/100parts_10steps_5res_5poses_8exh_v1_top100PRTs_${time_hash}.log
